44593a7296de5cb1a9ff08e8f18c3897fdc170fb
Jakub Burzynski <jakubburzynski@mat.umk.pl>
2025-04-13 12:12:41 +0200

    dodanie funkcji silnia

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,14 @@
 
 using namespace std;
 
+long silnia(int n) {
+	long s = 1;
+	for (int i = n; i > 1; i--) {
+		s *= i;
+	}
+	return s;
+}
+
 int main() {
     int a, b;
     cout << "Podaj liczbe calkowita ";
