dfaf49c4af5f88a3b91509890d6849b9bc380fd8
Jakub Burzynski <jakubburzynski@mat.umk.pl>
2025-04-13 12:18:17 +0200

    dodanie funkcji czyPierwsza

--- projekt1.cpp
+++ projekt1.cpp
@@ -2,6 +2,20 @@
 
 using namespace std;
 
+bool czyPierwsza(int a) {
+	if (a < 2) {
+		return false;
+	}
+
+	for (int i = 2; i * i <= a; i++) {
+		if (a % i == 0) {
+			return false;
+		}
+	}
+
+	return true;
+}
+
 int main() {
     int a, b;
     cout << "Podaj liczbe calkowita ";
