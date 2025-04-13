90dfcc74e0544b8eb57ca14d29d788a57c00c329
Jakub Burzynski <jakubburzynski@mat.umk.pl>
2025-04-13 12:04:29 +0200

    dodanie projekt1

---
+++ projekt1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a, b;
+    cout << "Podaj liczbe calkowita ";
+    cin >> a >> b;
+    int wyjscie;
+    do {
+        cout << endl;
+        cout << endl;
+        cout << "MENU" << endl;
+        cout << "Podaj numer czynnosci, ktora chcesz wykonac" << endl;
+        cout << "0. Wyjscie" << endl;
+        cin >> wyjscie;
+    } while(wyjscie != 0);
+    return 0;
+}
